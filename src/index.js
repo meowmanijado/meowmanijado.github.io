addf87948ba10fe817b02d87280f50a64f4e3753
meowmanijado <sharmainemanijado@gmail.com>
2017-09-22 20:19:38 +0800

    Add npm run watch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import _ from 'lodash';
 import updateBackground from './updateBackground.js';
+import style from './style.css'
+
+
 
 function component() {
     var element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
 	updateBackground();
 
+	var windowHeight = window.innerHeight - 40;
+    document.body.style.height = windowHeight + "px";
+
+	function setWindowHeight(){
+	    var windowHeight = window.innerHeight - 40;
+	    document.body.style.height = windowHeight + "px";
+	}
+	
+	window.addEventListener("resize",setWindowHeight,false);
+
 	window.addEventListener('click', updateBackground);
+	
 
     return element;
 }
